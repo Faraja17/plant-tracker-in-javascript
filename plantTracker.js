4981a0da3771db47e04b7a8cf82011c747bb1018
Faraja17 <farajakthompson@gmail.com>
2022-05-30 02:28:50 -0400

    added a while loop
    tested loop, tested the push and pop.  They work!

--- plantTracker.js
+++ plantTracker.js
@@ -1,14 +1,22 @@
 const indoorPlants = [];
 const outdoorPlants = [];
+let welcome = prompt("Welcome to your Plant Tracker!  Do you wish to add or remove a plant (yes or no)?").toLowerCase();
 
-
-let typePrompt = prompt("Is your plant indoor or outdoor?");
-let type = typePrompt.toLowerCase();
-let actionPrompt = prompt("Do you wish to add or remove the plant?");
-let action = actionPrompt.toLowerCase();
-let name = prompt("What is the name of your plant?")
+function getPlantInfo() {
+    while (welcome === "yes") {
+        addRemovePlants();
+        welcome = prompt("Do you wish to add or remove another plant (yes or no)?")
+    }
+    alert("Have a nice day!");
+}
 
 function addRemovePlants() {
+let typePrompt = prompt("Is your plant indoor or outdoor?");
+type = typePrompt.toLowerCase();
+let actionPrompt = prompt("Do you wish to add or remove the plant?");
+action = actionPrompt.toLowerCase();
+let name = prompt("What is the name of your plant?");
+
     if (type === "indoor" && action === "add") {
         indoorPlants.push(name);
     }
@@ -23,10 +31,10 @@ function addRemovePlants() {
     } else {
         alert("Have a nice day!");
     }
-    console.log("Indoor Plants:")
+    console.log("Indoor Plants:");
     console.log(indoorPlants);
-    console.log("Outdoor Plants:")
+    console.log("Outdoor Plants:");
     console.log(outdoorPlants);
 }
 
-addRemovePlants();
+getPlantInfo();
